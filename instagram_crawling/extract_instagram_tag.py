92497bb63bdc66f6d37da08a861bba1beec563d5
shc5529 <shc5529@naver.com>
2022-08-24 20:02:39 +0900

    delete test code 및 extract_count+=1, post_count+=1의 위치 변경

--- instagram_crawling/extract_instagram_tag.py
+++ instagram_crawling/extract_instagram_tag.py
@@ -49,12 +49,8 @@ def duplicate_count_and_make_dict(data_list, data_dict):
 	''' list 중복 카운트하여 결과 dict에 추가하는 함수 '''
 
 	for i in data_list:
-		try: 
-			data_dict[i] += 1
-			print(data_dict[i])
-		except: 
-			data_dict[i] = 1
-			print(data_dict[i])
+		try: data_dict[i] += 1
+		except: data_dict[i] = 1
 
 def extract_hash_tag(args, file_path=None):
 	''' 게시글에서 해쉬태그 추출하는 함수 '''
@@ -93,6 +89,9 @@ def extract_hash_tag(args, file_path=None):
 			# 에러 발생시 확인시 True로 변경
 			extract_error = False
 
+			# 인스타 추출 블락 방지 (특정 갯수 이상일시 True로 변경)
+			extract_safe = False
+
 			if int(post_urls_num) == int(extract_count):
 				print('---------- 해당 게시글 url 엑셀은 이미 추출이 완료되었습니다. ---------- \n')
 				sys.exit('---------- 다시 추출을 원하시면 해당 엑셀 파일의 해쉬태그 추출 횟수를 0을 저장 후 다시 시도해주세요. ---------- \n')
@@ -120,6 +119,9 @@ def extract_hash_tag(args, file_path=None):
 						title = soup.select_one(HASH_TAG_TITLE_TAG).string
 						tag_list += re.findall('#[A-Za-z0-9가-힣]+', title)
 
+						extract_count += 1
+						post_count += 1
+
 						# 게시글 url 엑셀 파일에 태그 추출 횃수 업데이트
 						update_excel(post_url_file_path, 'post_url', extract_count)
 
@@ -146,29 +148,27 @@ def extract_hash_tag(args, file_path=None):
 
 							# 결과 엑셀로 생성
 							export_excel(hash_tag, 'tag_name', tag_dict, file_date)
-
-						extract_count += 1
-						post_count += 1
 					else:
 						# 정상 응답이 아니면 extract_error 변수값 True려 변경 후 for문 종료
-						extract_error = True
-
 						print('---------- 인스타그램 봇 차단에 의해서 해쉬태그 추출이 끊어졌습니다. 잠시 후 다시 시도해주세요. ---------- \n')
 						print('---------- 문제가 지속된다면 개발자에게 문의해주세요. ---------- \n')
 
+						extract_error = True
 						break
 
-					if post_count >= int(args.ectract):
-						# extract_safe = True
+					if int(args.ectract) >= 100 and post_count >= int(args.ectract):
 						print(f'---------- 인스타그램 봇 차단 방지를 위해서 게시글 {args.ectract}개 까지만 확인합니다. ---------- \n')
+
+						extract_safe = True
 						break
 
 				# extract_error 혹은 extract_safe 변수값 True이면 while문 종료
-				if extract_error:
+				if extract_error or extract_safe:
 					break
 
 			print('---------- 게시글에서 해쉬태그 추출이 완료 되었습니다. ---------- \n')
 			return import_excel(tag_name_file_path, 'tag_name')['tag_count']
+			
 		else:
 			print('error: 게시글 해쉬태그 추출을 위한 로그인에 실패했습니다. 잠시 후 다시 시도해주세요.\n')
 			sys.exit('문제가 지속된다면 개발자에게 문의해주세요. \n')
